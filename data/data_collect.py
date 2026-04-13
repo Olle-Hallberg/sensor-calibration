b27052b8ac7f56d5592406b4485a275de771871b
Olle Hallberg <olle.hallberg02@gmail.com>
2026-04-13 10:46:41 +0200

    Updated data_collect.

--- data/data_collect.py
+++ data/data_collect.py
@@ -32,7 +32,7 @@ CH4_1 = 1796.08 # CORE-1
 CH4_2 = 1967.2  # Tek. luft
 CH4_3 = 2194.89 # CORE-2
 CH4_4 = 2298.1  # Atm. luft
-CH4_levels = [CH4_4, CH4_3, CH4_2, CH4_1]
+CH4_levels = [CH4_1, CH4_2, CH4_3, CH4_4]
 CH4_segment = t_max / len(CH4_levels)               # duration of each CH4 level in seconds
 CH4_idx = (t // CH4_segment).astype(int)            # index for CH4 level based on time
 CH4_idx = np.clip(CH4_idx, 0, len(CH4_levels) - 1)  # ensure index is within bounds
@@ -41,6 +41,10 @@ CH4 = np.array(CH4_levels)[CH4_idx]                 # CH4 concentration profile 
 # CO2 [ppm]
 CO2_2 = 435.8  # Tek. luft
 CO2_4 = 450.1  # Atm. luft
+CO2_levels = [0, CO2_2, 0, CO2_4]
+CO2_idx = (t // CH4_segment).astype(int)            # index of each CO2 segment based on time
+CO2_idx = np.clip(CO2_idx, 0, len(CO2_levels) - 1)  # ensure index is within bounds
+CO2 = np.array(CO2_levels)[CO2_idx]                 # CO2 concentration profile over time
 
 # Temperature [°C]
 T_levels = np.linspace(T_min, T_max, T_num_levels)
@@ -61,7 +65,7 @@ H_idx = np.clip(H_idx, 0, len(H_cycle) - 1)             # ensure index is within
 H = np.array(H_cycle)[H_idx]                            # Humidity profile over time, cycling through H levels within each T segment
 
 ### Plot ###
-fig, (ax11, ax2) = plt.subplots(2, 1, figsize=(14, 7))
+fig, (ax11, ax21) = plt.subplots(2, 1, figsize=(14, 7))
 
 ## Temperature and Humidity over Time ##
 # Temperature (left axis)
@@ -80,16 +84,25 @@ ax11.set_xlabel("Time [s]")
 ax11.set_title("Temperature and Humidity over Time")
 ax11.grid(True)
 
-## CH4 over Time ##
+## CH4 and CO2 over Time ##
 # CH4 (left axis)
-ax2.plot(t, CH4, color='green', linewidth=2)
-ax2.set_ylabel("CH4 [ppb]", color='green')
-ax2.tick_params(axis='y', labelcolor='green')
+ax21.plot(t, CH4, color='green', linewidth=2)
+ax21.set_ylabel("CH4 [ppb]", color='green')
+ax21.tick_params(axis='y', labelcolor='green')
+
+# CO2 (right axis)
+ax22 = ax21.twinx()
+ax22.plot(t, CO2, color='orange', linewidth=2)
+ax22.set_ylabel("CO2 [ppm]", color='orange')
+ax22.tick_params(axis='y', labelcolor='orange')
 
 # Labels and more
-ax2.set_xlabel("Time [s]")
-ax2.set_title("CH4 over Time")
-ax2.grid(True)
+ax21.set_zorder(2)
+ax22.set_zorder(1)
+ax21.patch.set_visible(False)
+ax21.set_xlabel("Time [s]")
+ax21.set_title("CH4 and CO2 over Time")
+ax21.grid(True)
 
 # Adjust layout and show plot
 plt.tight_layout()
