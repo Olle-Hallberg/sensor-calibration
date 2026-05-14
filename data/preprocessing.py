870dd3c338ecb65355adf4970c638c825704e2bc
Olle Hallberg <olle.hallberg02@gmail.com>
2026-05-14 10:01:53 +0200

    Added new preprocessed data files and updated preprocessing script.

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-### dataframe ###
-df = pd.read_csv('teknisk luft/Preprocessed/data_teknisk_luft.csv')
+### DataFrame ###
+df = pd.read_csv('data/preprocessed/teknisk_luft/data_teknisk_luft.csv')
 # print(f'data.shape: {df.shape}')
 # df.info()
 # print(df.describe())
@@ -14,9 +14,13 @@ C_air = ((df['mpx5500_pressure_hpa'] + 1013) * 100) / (8.314 * (df['rh_temp_c'] 
 C_CH4 = 1967.2/1000 * C_air
 C_CO2 = 435.8 * C_air
 
-### COncentration columns ###
+### Concentration columns ###
 df['ch4_conc_mol_m3'] = C_CH4
 df['co2_conc_mol_m3'] = C_CO2
 
+### reference gas molar fraction ###
+df['ch4_ppm'] = 1967.2/1000
+df['co2_ppm'] = 435.8
+
 ### Save ###
-df.to_csv('teknisk luft/Preprocessed/data_teknisk_luft.csv', index=False)
+df.to_csv('data/preprocessed/teknisk_luft/data_teknisk_luft.csv', index=False)
