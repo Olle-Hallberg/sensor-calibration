0925d7790f2881535594de3457057c705980d8d9
Olle Hallberg <olle.hallberg02@gmail.com>
2026-05-14 10:28:44 +0200

    modified:   data/preprocessing.py

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 ### DataFrame ###
-df = pd.read_csv('data/preprocessed/teknisk_luft/data_teknisk_luft.csv')
+# df = pd.read_csv('data/preprocessed/teknisk_luft/data_teknisk_luft.csv')
 # print(f'data.shape: {df.shape}')
 # df.info()
 # print(df.describe())
@@ -23,4 +23,4 @@ df['ch4_ppm'] = 1967.2/1000
 df['co2_ppm'] = 435.8
 
 ### Save ###
-df.to_csv('data/preprocessed/teknisk_luft/data_teknisk_luft.csv', index=False)
+# df.to_csv('data/preprocessed/teknisk_luft/data_teknisk_luft.csv', index=False)
